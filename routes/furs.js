b3e6e3fe01274eedbe2c3c53eb82c275b258a507
crts <crt.sajnic@gmail.com>
2020-04-21 20:48:35 +0200

    tls1.2

--- routes/furs.js
+++ routes/furs.js
@@ -10,31 +10,32 @@ const fursUrl = {
 	path: '/v1/getInvoice?qr={qr}&apikey={apiKey}',
 	method: 'GET',
 	//key: fs.readFileSync("/srv/www/keys/my-site-key.pem"),
-  	cert: fs.readFileSync("/root/test-hello-world/test-tls.cer")
+	cert: fs.readFileSync("/root/test-hello-world/test-tls.cer"),
+	secureProtocol: "TLSv1_2_method"
 }
 
 router.get('/', function (req, res, next) {
 	let apiKey = 'ZaDEKtHoSdGa0P4TwWV3tm6FkwUo71GL';
 	let qrCode = req.query.code;
 
-	if(qrCode != null && qrCode.length == 4) {
+	if (qrCode != null && qrCode.length == 4) {
 		fursUrl.path = fursUrl.path.replace('{qr}', qrCode);
 		fursUrl.path = fursUrl.path.replace('{apiKey}', apiKey);
-	
+
 		console.log(fursUrl);
 
 		const reqq = https.request(fursUrl, ress => {
 			console.log(`statusCode: ${ress.statusCode}`)
-	
+
 			ress.on('data', d => {
 				res.json(error);
 			})
 		})
-	
+
 		reqq.on('error', error => {
 			res.json(error);
 		})
-	
+
 		reqq.end()
 	} else {
 		res.json({ code: 234 });
