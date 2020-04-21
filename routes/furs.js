c4f1b4cd13ae865156b695e9a8ea02c1dd6d825a
crts <crt.sajnic@gmail.com>
2020-04-21 20:39:29 +0200

    setting up 6

--- routes/furs.js
+++ routes/furs.js
@@ -10,36 +10,35 @@ const fursUrl = {
 	path: '/v1/getInvoice?qr={qr}&apikey={apiKey}',
 	method: 'GET',
 	//key: fs.readFileSync("/srv/www/keys/my-site-key.pem"),
-  	ca: fs.readFileSync("/root/test-hello-world/test-tls.cer")
+  	cert: fs.readFileSync("/root/test-hello-world/test-tls.cer")
 }
 
 router.get('/', function (req, res, next) {
 	let apiKey = 'ZaDEKtHoSdGa0P4TwWV3tm6FkwUo71GL';
 	let qrCode = req.query.code;
 
-	//if(qrCode != null && qrCode.length == 4) {
-	//	fursUrl.path = fursUrl.path.replace('{qr}', qrCode);
-	//	fursUrl.path = fursUrl.path.replace('{apiKey}', apiKey);
-	//
-	//	res.json(fursUrl);
-	//} else {
-	//	res.json({ code: 234 });
-	//}
+	if(qrCode != null && qrCode.length == 4) {
+		fursUrl.path = fursUrl.path.replace('{qr}', qrCode);
+		fursUrl.path = fursUrl.path.replace('{apiKey}', apiKey);
+	
+		console.log(fursUrl);
 
-	const reqq = https.request(fursUrl, ress => {
-		console.log(`statusCode: ${ress.statusCode}`)
-
-		ress.on('data', d => {
+		const reqq = https.request(fursUrl, ress => {
+			console.log(`statusCode: ${ress.statusCode}`)
+	
+			ress.on('data', d => {
+				res.json(error);
+			})
+		})
+	
+		reqq.on('error', error => {
 			res.json(error);
 		})
-	})
-
-	reqq.on('error', error => {
-		res.json(error);
-	})
-
-	reqq.end()
-
+	
+		reqq.end()
+	} else {
+		res.json({ code: 234 });
+	}
 });
 
 module.exports = router;
