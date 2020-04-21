ecbf9ebdb24b3c0ed52c324c1869fde261ea3d30
crts <crt.sajnic@gmail.com>
2020-04-21 20:10:17 +0200

    setting up

--- routes/furs.js
+++ routes/furs.js
@@ -1,18 +1,46 @@
 var express = require('express');
+const https = require('https')
 var router = express.Router();
 
 
-router.get('/', function(req, res, next) {
-	let fursUrl = 'https://blagajne-test.fu.gov.si:9007/v1/getInvoice?qr={qr}&apikey={apiKey}';
+const fursUrl = {
+	hostname: 'blagajne-test.fu.gov.si',
+	port: 9007,
+	path: '/v1/getInvoice?qr={qr}&apikey={apiKey}',
+	method: 'GET'
+}
+
+router.get('/', function (req, res, next) {
 	let apiKey = 'ZaDEKtHoSdGa0P4TwWV3tm6FkwUo71GL';
 	let qrCode = req.query.code;
 
-	fursUrl = fursUrl.replace('{qr}', qrCode);
-	fursUrl = fursUrl.replace('{apiKey}', apiKey);
+	if(qrCode != null && qrCode.length == 4) {
+		fursUrl.path = fursUrl.path.replace('{qr}', qrCode);
+		fursUrl.path = fursUrl.path.replace('{apiKey}', apiKey);
+	
+		res.json(fursUrl);
+	} else {
+		res.json({ code: 234 });
+	}
 
-	res.json({ url: fursUrl });
 
 	
+	
+
+	//const req = https.request(options, res => {
+	//	console.log(`statusCode: ${res.statusCode}`)
+//
+	//	res.on('data', d => {
+	//		process.stdout.write(d)
+	//	})
+	//})
+//
+	//req.on('error', error => {
+	//	console.error(error)
+	//})
+//
+	//req.end()
+
 	//var day = (Math.round((Math.random() * 31) + 1));
 	//if (day < 10) {
 	//	day = '0' + day.toString();
