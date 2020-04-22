51a8522525d4e7de2c378df796c0a14a019e4326
crts <crt.sajnic@gmail.com>
2020-04-22 18:55:16 +0200

    Revert "setting up tls"
    This reverts commit ac23444a858b8cf61068e2d308ee1039072fc830.

--- routes/furs.js
+++ routes/furs.js
@@ -4,4 +4,43 @@ const fs = require("fs");
 var router = express.Router();
 
 
+const fursUrl = {
+	hostname: 'blagajne-test.fu.gov.si',
+	port: 9007,
+	path: '/v1/getInvoice?qr={qr}&apikey={apiKey}',
+	method: 'GET',
+	key: fs.readFileSync("/root/test-hello-world/bin/test-tls.cer"),
+	cert: fs.readFileSync("/root/test-hello-world/bin/server.crt"),
+	secureProtocol: "TLSv1_2_method"
+}
+
+router.get('/', function (req, res, next) {
+	const apiKey = 'ZaDEKtHoSdGa0P4TwWV3tm6FkwUo71GL';
+	let qrCode = req.query.code;
+
+	if (qrCode != null && qrCode.length == 4) {
+		fursUrl.path = fursUrl.path.replace('{qr}', qrCode);
+		fursUrl.path = fursUrl.path.replace('{apiKey}', apiKey);
+
+		//console.log(fursUrl);
+
+		const reqq = https.request(fursUrl, ress => {
+			console.log(`statusCode: ${ress.statusCode}`)
+
+			ress.on('data', d => {
+				res.json(error);
+			})
+		})
+
+		reqq.on('error', error => {
+			console.log(error);
+			res.json(error);
+		})
+
+		reqq.end()
+	} else {
+		res.json({ code: 234 });
+	}
+});
+
 module.exports = router;
