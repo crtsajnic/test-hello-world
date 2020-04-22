017e46fadcaf4e8f56aa0c01f6e9a7d580b70046
crts <crt.sajnic@gmail.com>
2020-04-22 19:20:12 +0200

    tls setup

--- routes/furs.js
+++ routes/furs.js
@@ -33,7 +33,7 @@ router.get('/', function (req, res, next) {
 			ress.on('data', d => {
 				//res.json({ data: true });
 				let json = JSON.parse(d);
-				console.log(json['data'].toString('utf8'));
+				console.log(d);
 				res.json(json);
 			})
 		})
